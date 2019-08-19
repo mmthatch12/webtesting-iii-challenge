43eae3162711a60d6c0d6de58bd3c802d10385eb
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-08-19 14:18:13 -0600

    testing display.js

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,12 @@
-// Test away!
+import React from 'react'
+import renderer from 'react-test-renderer'
+import Display from './Display'
+import { render } from 'react-testing-library';
+
+describe('<Display />', () => {
+    it('shoes the controls and display', () => {
+        const tree = renderer.create(<Display />);
+
+        expect(tree.toJSON()).toMatchSnapshot()
+    })
+})
